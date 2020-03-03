b1e20178b36a70abb9ca69b044994ed561973499
zzpapy <gregory.pace@hotmail.fr>
2020-03-03 17:19:39 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="public/styles/style.css">
+</head>
+<body>
+<div class="wrapper">
+    <header class="header">       
+            <figure class="img_logo">
+                <img src="public/images/logo.png" alt="">
+            </figure>       
+            <nav class="nav">
+                <a href="#">HOME</a>
+                <a href="#">ABOUT</a>
+                <a href="#">PORTFOLIO</a>
+                <a href="#">TESTIMONIALS</a>
+                <a href="#">CONTACT</a>
+                <a href="#">PAGES</a>
+            </nav>        
+    </header>
+    <div class="main">
+        <section class="top">
+            <h1 class="titre_top">HTML TEMPLATE FOR CREATIVE FOLKS AND DESIGN AGENCIES</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis ab cumque iure iusto, perspiciatis maiores maxime provident. Accusamus, facere consectetur eius, cupiditate tempore neque quibusdam perspiciatis dolorem, quia saepe nihil!</p>
+        </section>
+        <section class="explain">
+            <dl>
+                <h2>Why Choose Us ?</h2>
+                <dt>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, accusantium?</dt>
+                <dt>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, neque.</dt>
+            </dl>
+            <dl>
+                <h2>What you get ?</h2>
+                <dt><h3><i class="fab fa-wordpress"></i>COLORFUL & COMPACT</h3></dt>
+                <dt><h3><i class="fas fa-cog"></i>VERY EASY TO USE</h3></dt>
+                <dt><h3><i class="fab fa-telegram-plane"></i>CODED WITH SASS</h3></dt>
+            </dl>
+            <dl>
+                <dt class="accord deroul">
+                    <i class="fas fa-plus " ></i>Lorem ipsum dolor sit amet.
+                    <dl class="cache hide">
+                        <dt>Lorem ipsum dolor sit amet.</dt>
+                    </dl>
+                </dt>
+                <dt class="accord deroul">
+                    <i class="fas fa-plus "></i>Lorem ipsum dolor sit amet
+                    <dl class="cache hide">
+                        <dt>Lorem ipsum dolor sit amet.</dt>
+                    </dl>
+                </dt>
+                <dt class="accord deroul">
+                    <i class="fas fa-plus "></i>Lorem ipsum dolor sit amet
+                    <dl class="cache hide">
+                        <dt>Lorem ipsum dolor sit amet.</dt>
+                    </dl>
+                </dt>
+            </dl>
+        </section>
+        <section class="gal">
+            <div class="figures">
+                <figure class="img_gal"><i class="fas fa-music"></i><img src="public/images/logo.png" alt=""></figure>
+                <figure class="img_gal"><i class="far fa-play-circle"></i><img src="public/images/logo.png" alt=""></figure>
+                <figure class="img_gal"><img src="public/images/logo.png" alt=""></figure>
+                <figure class="img_gal"><img src="public/images/logo.png" alt=""></figure>
+                <figure class="img_gal"><img src="public/images/logo.png" alt=""></figure>
+                <figure class="img_gal"><img src="public/images/logo.png" alt=""></figure>
+                <figure class="img_gal"><img src="public/images/logo.png" alt=""></figure>
+                <figure class="img_gal"><img src="public/images/logo.png" alt=""></figure>
+            </div>
+            <div class="foot_gal">
+                <div class="nav_gal">
+                    <a href="#">ALL</a>
+                    <a href="#">AUDIO</a>
+                    <a href="#">IMAGE</a>
+                    <a href="#">VIDEO</a>
+                </div>
+                <div class="more">
+                    <a href="#">LOAD MORE</a>
+                </div>
+            </div>
+        </section>
+        <section class="comment">
+            <h1>CLIENT TESTIMONIALS</h1>
+            <div class="container">
+                <div class="avatar">
+                    <img src="public/images/moi.jpeg" alt="Avatar" style="width:90px">
+                </div>
+                <q><span>Chris Fox.</span> CEO at Mighty Schools.</q>
+                <q>John Doe saved us from a web disaster.</q>
+            </div>
+            <div class="container">
+                <q><span>Chris Fox.</span> CEO at Mighty Schools.</q>
+                <q>John Doe saved us from a web disaster.</q>
+                <div class="avatar">
+                    <img src="public/images/moi.jpeg" alt="Avatar" style="width:90px">
+                </div>
+            </div>
+            <div class="container">
+                <div class="avatar">
+                    <img src="public/images/moi.jpeg" alt="Avatar" style="width:90px">
+                </div>
+                <q><span>Chris Fox.</span> CEO at Mighty Schools.</q>
+                <q>John Doe saved us from a web disaster.</q>
+            </div>
+            <div class="become">
+                <a href="">BECOME OUR CLIENT</a>
+            </div>            
+        </section>
+        <section class="horizon">
+            <h1>NEW HORIZONS AWAITE</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam facere voluptates ipsum ipsa inventore quo, doloribus magni sapiente autem! Ab cupiditate eveniet hic animi debitis voluptate neque molestias quia voluptas.
+            </p>
+            <a href="">BUY VIBES THEMEFOREST</a>
+
+        </section>
+        <section class="contact">
+            <h1>GET IN TOUCH</h1>
+            <div>
+                <form action="">
+                    <div>
+                        <label for="nom">Nom</label>
+                        <input id="nom" type="text">
+                        <label for="mail">Email</label>
+                        <input id="mail" type="text">
+                        <label for="tel">Téléphone</label>
+                        <input id="tel" type="text">
+                    </div>
+                    <div>
+                        <label for="sujet">Sujet</label>
+                        <input id="sujet" type="text">
+                        <label for="area">Message</label>
+                        <textarea id="area" type="text"></textarea>
+                        <input type="submit" value="ENVOYER">
+                    </div>
+                </form>
+            </div>
+        </section>
+    </div>
+    <footer class="footer">
+        <div class="footer_div">
+            <h2>ABOUTE VIBES</h2>
+        </div>
+        <div class="footer_div">
+            <h2>NEWSLETTER</h2>
+            <P>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, corporis tenetur, <strong>dolorum accusamus placeat provident cupiditate nulla molestias, eum quo delectus minus suscipit dignissimos fugit sequi odit! Corrupti, </strong>praesentium veritatis.
+            </P>
+            <P></P>
+        </div>
+        <div class="footer_div">
+            <h2>TWITER FEED</h2>
+        </div>
+    </footer>
+</div>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="public/scripts/script.js"></script>
+</body>
+</html>
---
+++ public/scripts/script.js
@@ -0,0 +1,5 @@
+$(".deroul").on("click",function(){
+    $(this).next().toggle('hide')
+    // $(this).addClass('block')
+})
+
